02f9c1325c468452ed45c9ee232381966c0e255c
Danilo Radovic <daniloradovic@danilos-air.lan>
2019-08-25 16:51:11 -0700

    made program more generalized to apply to any subreddit and query

--- graphing.py
+++ graphing.py
@@ -1,17 +1,20 @@
-import json, time, pprint, math
+import sys, json, time, pprint, math
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.mlab as mlab
 from datetime import datetime
-#dont use pylab, use pyplot
-#important dates
-#09/17/2018, 09/29/2018, 11/23/2018
 
+sub_reddit = sys.argv[1]
 
-with open('posts.json') as p:
+file_name = sys.argv[2]
+extension_position = file_name.find('.json')
+if(extension_position != -1):
+    file_name = file_name[:extension_position]
+
+with open(file_name + '_posts.json') as p:
     json_data_posts = json.load(p)
 
-with open('comments.json') as c:
+with open(file_name + '_comments.json') as c:
     json_data_comments = json.load(c)
 
 def sortFirst(val):
@@ -23,46 +26,53 @@ for post in json_data_posts['posts']:
 
 dates_comments = []
 for comment in json_data_comments['comments']:
-    dates_comments.append((int(comment['created_utc']), float(comment['sentiment'])))
+    dates_comments.append((int(comment['created_utc']),  \
+                           float(comment['sentiment'])))
 
+#Sort by date
 dates_posts.sort(key=sortFirst, reverse=False)
 dates_comments.sort(key=sortFirst, reverse=False)
-bar_time = []   #x-axis
-bar_sentiment = []  #y-axis
-sentiment_posts = []
-time_comments = []
-sentiment_comments = []
 
-def populate_bar(time_type, sentiment_type, arr):
+bar_date = []           #x-axis
+bar_sentiment = []      #y-axis
+#Temporary arrays before they are averaged
+post_sentiment = []
+comment_dates = []
+comment_sentiment = []
+
+def populate_bar(time, sentiment, arr):
+
     start_index = 0
     for i in range(len(arr)):
-        start_interval_date = datetime.utcfromtimestamp(arr[start_index][0]).date()
+        start_date = datetime.utcfromtimestamp(arr[start_index][0]).date()
         current_date = datetime.utcfromtimestamp(arr[i][0]).date()
-        difference = current_date - start_interval_date
+        difference = current_date - start_date
         if difference.days > 6:
             interval = []
-            for j in range(start_index,i):
+            for j in range(start_index, i):
                 interval.append(arr[j][1])
 
-            time_type.append(str(start_interval_date))
-            interval_sentiment = np.average(interval)
-            sentiment_type.append(interval_sentiment)
+            time.append(str(start_date))
+            avg_sentiment = np.average(interval)
+            sentiment.append(avg_sentiment)
             start_index = i
 
-populate_bar(bar_time, sentiment_posts, dates_posts)
-populate_bar(time_comments, sentiment_comments, dates_comments)
 
-for i in range(len(sentiment_posts)):
-    bar_sentiment.append((sentiment_posts[i]+sentiment_comments[i])/2)
+populate_bar(bar_date, post_sentiment, dates_posts)
+populate_bar(comment_dates, comment_sentiment, dates_comments)
 
-y_pos = np.arange(len(bar_time))
+for i in range(len(post_sentiment)):
+    bar_sentiment.append((post_sentiment[i] + comment_sentiment[i]) / 2)
+
+y_pos = np.arange(len(bar_date))
 bar = plt.bar(y_pos, bar_sentiment, align='center', alpha=0.5, color="purple")
 
-plt.title('Yandhi sentiment on r/Kanye (1 bar = average sentiment over a week)')
+plt.title(file_name + ' sentiment on r/' + sub_reddit +     \
+          ' (1 bar = average sentiment in a week)')
 plt.xlabel('Dates of posts & comments')
 plt.ylabel('Sentiment (-1.0 to 1.0)')
-# plt.xticks([0,10,20,30], ['a', 'b', 'c', 'd'])
-plt.xticks(range(0,len(bar_time),5), bar_time[0:len(bar_time):5])
-plt.tick_params(axis='x',which='both',bottom=True,top=False,labelbottom=True, labelrotation=15)
+plt.xticks(range(0, len(bar_date), 5), bar_date[0:len(bar_date):5])
+plt.tick_params(axis='x', which='both', bottom=True, top=False,    \
+                labelbottom=True, labelrotation=15)
 
 plt.show()
--- redditcollector.py
+++ redditcollector.py
@@ -1,18 +1,27 @@
 #Python3
-import praw, psaw, json, time, redis
+import sys, praw, psaw, json, time
 from textblob import TextBlob
 from datetime import datetime
 from psaw import PushshiftAPI
 
-#activate python environment: $ source env/bin/activate
-#install stuff and work inbetween
-#deactivate python environment: $ deactivate
+sub_reddit = sys.argv[1]
+query_name = sys.argv[2]
 
-#my client_id, client_secret, and user_agent are in my praw.ini file
+file_name = sys.argv[3]
+extension_position = file_name.find('.json')
+if(extension_position != -1):
+    file_name = file_name[0:extension_position]
+
+month = int(sys.argv[4])
+day = int(sys.argv[5])
+year = int(sys.argv[6])
+
+
+#My client_id, client_secret, and user_agent are in my private praw.ini file
 reddit = praw.Reddit('bot1')
 api = PushshiftAPI(reddit)
-#date the album was announced
-start_date = int(datetime(2018, 9, 17).timestamp())
+#Date the album was announced
+start_date = int(datetime(year, month, day).timestamp())
 
 post_data = {}
 post_data["posts"] = []
@@ -20,32 +29,39 @@ post_data["posts"] = []
 comment_data = {}
 comment_data["comments"] = []
 
-for entry in api.search_submissions(after=start_date, subreddit='Kanye', q='Yandhi'):
-    tsent = TextBlob(entry.title)
-    stsent = TextBlob(entry.selftext)
-    if tsent.sentiment.polarity != 0 or tsent.sentiment.subjectivity != 0 or stsent.sentiment.polarity != 0 or stsent.sentiment.subjectivity != 0:
-        #if analyzer could gather anything at all, include data point
-        new_entry = {"id":entry.id,
-                     "title":entry.title,
-                     "selftext":entry.selftext,
-                     "created_utc":str(int(entry.created_utc)),
-                     "sentiment":str(max(tsent.sentiment.polarity,stsent.sentiment.polarity))
+for entry in api.search_submissions(after=start_date, subreddit=sub_reddit, \
+                                    q=query_name):
+    tsent = TextBlob(entry.title)       #Title sentiment
+    stsent = TextBlob(entry.selftext)   #Selftext sentiment
+    if tsent.sentiment.polarity != 0            \
+        or tsent.sentiment.subjectivity != 0    \
+        or stsent.sentiment.polarity != 0       \
+        or stsent.sentiment.subjectivity != 0:
+        #If analyzer could gather anything at all, include data point
+        new_entry = {"id" : entry.id,
+                     "title" : entry.title,
+                     "selftext" : entry.selftext,
+                     "created_utc" : str(int(entry.created_utc)),
+                     "sentiment" : str(max(tsent.sentiment.polarity,    \
+                                         stsent.sentiment.polarity))
                      }
         post_data["posts"].append(new_entry)
 
-for entry in api.search_comments(after=start_date, subreddit='Kanye', q='Yandhi'):
-    bsent = TextBlob(entry.body)
+
+for entry in api.search_comments(after=start_date, subreddit='Kanye', \
+                                 q='Yandhi'):
+    bsent = TextBlob(entry.body)        #Body sentiment
     if bsent.sentiment.polarity != 0 or bsent.sentiment.subjectivity != 0:
-        new_entry = {"id":entry.id,
-                     "body":entry.body,
-                     "created_utc":str(int(entry.created_utc)),
-                     "sentiment":str(bsent.sentiment.polarity)
+        new_entry = {"id" : entry.id,
+                     "body" : entry.body,
+                     "created_utc" : str(int(entry.created_utc)),
+                     "sentiment" : str(bsent.sentiment.polarity)
                      }
         comment_data["comments"].append(new_entry)
 
 
-with open('posts.json', 'w') as yandhi:
+with open(file_name + '_posts.json', 'w') as yandhi:
         json.dump(post_data, yandhi, indent=2)
 
-with open('comments.json', 'w') as yandhi:
+with open(file_name + '_comments.json', 'w') as yandhi:
         json.dump(comment_data, yandhi, indent=2)
